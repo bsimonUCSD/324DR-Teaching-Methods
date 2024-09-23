e12466d18da25edb528811d9e7df782b84f11791
333GUSSS <gbaez@ucsd.edu>
2024-09-23 11:52:32 -0700

    Added VerticalFlip

---
+++ VerticalFlip/Main.java
@@ -0,0 +1,36 @@
+class Main {
+  public static void main(String[] args){
+    int [] [] greyPic = 
+    {
+      {140, 170, 190},
+      {180, 200, 160},
+      {200, 210, 222},
+      {200, 100, 110}
+    };
+
+    int temp;
+    int bottomRowIndex;
+
+    for (int i = 0; i < (greyPic.length/2); i++)
+    {
+      bottomRowIndex = (greyPic.length)-1-i;
+      for (int j = 0; j < greyPic[i].length; j++)
+      {
+        temp = greyPic[i][j];
+        greyPic[i][j] = greyPic[bottomRowIndex][j];
+        greyPic[bottomRowIndex][j] = temp;
+      }
+    }
+
+    //Print out
+    for (int i = 0; i < greyPic.length; i++)
+    {
+      for (int j = 0; j < greyPic[i].length; j++)
+      {
+        System.out.print(greyPic[i][j]+ " ");
+      }
+      System.out.println();
+    }
+
+  }
+}
